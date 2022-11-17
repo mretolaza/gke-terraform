af837deb6bd53f3b426a16557752737098e475dd
mretolaza <ret16339@uvg.ed.gt>
2022-11-16 18:58:06 -0600

    Hola mundo

--- app/index.js
+++ app/index.js
@@ -28,7 +28,7 @@ try {
 
 // app.use('/api/person', router);
 app.use('/health', (_, res) => {
-  res.status(200).json({ success: true, message: 'Administración de Sistemas 2.0' });
+  res.status(200).json({ success: true, message: 'Hola Bidkar :D' });
 });
 
 app.listen(port, () => {
